e42f935d12b1ac998ece010767c27f183b6a3cc2
kullanıcı.cs <88928414+BTKYilan@users.noreply.github.com>
2022-07-25 22:28:13 +0300

    Add files via upload

--- back.py
+++ back.py
@@ -1,8 +1,27 @@
 import os
+import platform
+
+def creation_date(path_to_file):
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return stat.st_mtime
+
+def just_create_file(dir):
+    f = open(dir, "x")
+    f.close()
+
 
 def err(code: int, dir):
     print('Error code ' + str(code))
     return dir
+
 
 def lexer(command):
     tokens = []
@@ -21,16 +40,29 @@ def lexer(command):
 
 def do(tokens: list, cur_directory: str):
     if len(tokens) == 1:
-        if tokens[0] == 'updir':
+        com = str(tokens[0])
+        if com == 'find':
+            dirs = []
+            a = 1
+            for f in os.listdir(cur_directory):
+                print('   ', str(a), ') ', f)
+                a += 1
+                dirs.append(cur_directory + '\\' + f)
+            try:
+                a = int(input('>> '))
+                return dirs[a - 1]
+            except:
+                return err(1, cur_directory)
+            return cur_directory
+        elif com == 'up':
             return str(os.path.abspath(os.path.join(cur_directory, os.pardir)))
-        else:
-            return err(0, cur_directory)
     elif len(tokens) == 2:
-        if tokens[0] == 'finddir':
-            if os.path.exists(tokens[1]):
-                return tokens[1]
+        com = str(tokens[0])
+        if com == 'find_disk':
+            if str(tokens[1]).find(':') > 0 and os.path.isdir(str(tokens[1])):
+                return str(tokens[1])
             else:
-                return err(2, cur_directory)
+                return err(1, cur_directory)
         else:
             return err(0, cur_directory)
     else:

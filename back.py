7a4bd89d735eede711400a0911efea4ba413ea37
kullanıcı.cs <88928414+BTKYilan@users.noreply.github.com>
2022-07-26 10:07:35 +0300

    Update back.py

--- back.py
+++ back.py
@@ -10,8 +10,6 @@ def creation_date(path_to_file):
         try:
             return stat.st_birthtime
         except AttributeError:
-            # We're probably on Linux. No easy way to get creation dates here,
-            # so we'll settle for when its content was last modified.
             return stat.st_mtime
 
 

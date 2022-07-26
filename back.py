55d6f0e24a2a5545f43dcbf51c0b68d247adba85
kullanıcı.cs <88928414+BTKYilan@users.noreply.github.com>
2022-07-26 10:07:11 +0300

    Add files via upload

--- back.py
+++ back.py
@@ -1,6 +1,7 @@
 import os
 import platform
 
+
 def creation_date(path_to_file):
     if platform.system() == 'Windows':
         return os.path.getctime(path_to_file)
@@ -9,7 +10,10 @@ def creation_date(path_to_file):
         try:
             return stat.st_birthtime
         except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
             return stat.st_mtime
+
 
 def just_create_file(dir):
     f = open(dir, "x")
@@ -40,20 +44,34 @@ def do(tokens: list, cur_directory: str):
     if len(tokens) == 1:
         com = str(tokens[0])
         if com == 'find':
-            dirs = []
-            a = 1
-            for f in os.listdir(cur_directory):
-                print('   ', str(a), ') ', f)
-                a += 1
-                dirs.append(cur_directory + '\\' + f)
-            try:
-                a = int(input('>> '))
-                return dirs[a - 1]
-            except:
-                return err(1, cur_directory)
-            return cur_directory
+            if os.path.isdir(cur_directory):
+                dirs = []
+                a = 1
+                for f in os.listdir(cur_directory):
+                    print('   ', str(a), ') ', f)
+                    a += 1
+                    dirs.append(cur_directory + '\\' + f)
+                try:
+                    a = int(input('>> '))
+                    return dirs[a - 1]
+                except:
+                    return err(1, cur_directory)
+            else:
+                return err(3, cur_directory)
         elif com == 'up':
             return str(os.path.abspath(os.path.join(cur_directory, os.pardir)))
+        elif com == 'start':
+            print('   Starting...')
+            if os.path.isfile(cur_directory):
+                if platform.system() == 'Windows':
+                    os.system('"' + cur_directory + '"')
+                elif platform.system() == 'Darwin':
+                    os.system('open "' + cur_directory + '"')
+                return cur_directory
+            else:
+                return err(2, cur_directory)
+        else:
+            return err(0, cur_directory)
     elif len(tokens) == 2:
         com = str(tokens[0])
         if com == 'find_disk':
@@ -65,6 +83,7 @@ def do(tokens: list, cur_directory: str):
             return err(0, cur_directory)
     else:
         return err(0, cur_directory)
+
 
 def run():
     cur_dir: str = os.path.expanduser('~')
